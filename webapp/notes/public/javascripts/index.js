645307477ff2df527d792f4ff39d5b1ac2cd8185
Stefan Eugster <stefan.eugster@gmail.com>
2016-06-17 00:54:23 +0200

    priority icon rendering

--- webapp/notes/public/javascripts/index.js
+++ webapp/notes/public/javascripts/index.js
@@ -1,100 +1,102 @@
-'use strict';
+;(function () {
+    'use strict';
 
-var templateSource = $('#tasklist-template').html();
-var template = Handlebars.compile(templateSource);
-var listData = [];
+    var templateSource = $('#tasklist-template').html();
+    var template = Handlebars.compile(templateSource);
+    var listData = [];
 
-// render list
-function renderList(data) {
-    $("#taskListWrapper").html(template(data));
-};
+    // render list
+    function renderList(data) {
+        $("#taskListWrapper").html(template(data));
+    };
 
-// load task list
-dataService.all(function (data) {
-    listData = data;
-    renderList(listData);
-});
+    // load task list
+    dataService.all(function (data) {
+        listData = data;
+        renderList(listData);
+    });
 
-// add note
-$("body").on("click", "#btnAddNote", function(event) {
-    event.preventDefault();
-    // goto add note
-    window.location = '/add';
-});
+    // add note
+    $("body").on("click", "#btnAddNote", function (event) {
+        event.preventDefault();
+        // goto add note
+        window.location = '/add';
+    });
 
-// edit note
-$("body").on("click", ".btnEditNote", function(event) {
-    event.preventDefault();
-    // goto update note
-    window.location = '/update/' + $(this).data("task-id");
-});
+    // edit note
+    $("body").on("click", ".btnEditNote", function (event) {
+        event.preventDefault();
+        // goto update note
+        window.location = '/update/' + $(this).data("task-id");
+    });
 
-// order notes by priority
-$("body").on("click", "#btnOrderByPriority", function(event) {
-    event.preventDefault();
-    $(this).siblings().removeClass("active");
-    $(this).addClass("active");
-    // sort list
-    renderList(listData.sort(function (note1, note2) {
-        return note1.priority < note2.priority;
-    }));
-});
+    // order notes by priority
+    $("body").on("click", "#btnOrderByPriority", function (event) {
+        event.preventDefault();
+        $(this).siblings().removeClass("active");
+        $(this).addClass("active");
+        // sort list
+        renderList(listData.sort(function (note1, note2) {
+            return note1.priority < note2.priority;
+        }));
+    });
 
-// order notes by finish date
-$("body").on("click", "#btnOrderByFinishDate", function(event) {
-    event.preventDefault();
-    $(this).siblings().removeClass("active");
-    $(this).addClass("active");
-    // sort list
-    renderList(listData.sort(function (note1, note2) {
-        var date1 = new Date(note1.finishDate);
-        var date2 = new Date(note2.finishDate);
-        return date1 > date2;
-    }));
-});
+    // order notes by finish date
+    $("body").on("click", "#btnOrderByFinishDate", function (event) {
+        event.preventDefault();
+        $(this).siblings().removeClass("active");
+        $(this).addClass("active");
+        // sort list
+        renderList(listData.sort(function (note1, note2) {
+            var date1 = new Date(note1.finishDate);
+            var date2 = new Date(note2.finishDate);
+            return date1 > date2;
+        }));
+    });
 
-// order notes by create date
-$("body").on("click", "#btnOrderByCreateDate", function(event) {
-    event.preventDefault();
-    $(this).siblings().removeClass("active");
-    $(this).addClass("active");
-    // sort list
-    renderList(listData.sort(function (note1, note2) {
-        var date1 = new Date(note1.createDate);
-        var date2 = new Date(note2.createDate);
-        return date1 > date2;
-    }));
-});
+    // order notes by create date
+    $("body").on("click", "#btnOrderByCreateDate", function (event) {
+        event.preventDefault();
+        $(this).siblings().removeClass("active");
+        $(this).addClass("active");
+        // sort list
+        renderList(listData.sort(function (note1, note2) {
+            var date1 = new Date(note1.createDate);
+            var date2 = new Date(note2.createDate);
+            return date1 > date2;
+        }));
+    });
 
-// show finished notes
-$("body").on("click", "#btnShowFinished", function(event) {
-    event.preventDefault();
-    // toggle
-    if ($("#btnShowFinished").data("show-finished")) {
-        $("#btnShowFinished").data("show-finished", false);
-        $("#btnShowFinished").text('Show finished');
-        // load finished notes
+    // show finished notes
+    $("body").on("click", "#btnShowFinished", function (event) {
+        event.preventDefault();
+        // toggle
+        if ($("#btnShowFinished").data("show-finished")) {
+            $("#btnShowFinished").data("show-finished", false);
+            $("#btnShowFinished").text('Show finished');
+            // load finished notes
+            // ???
+        } else {
+            $("#btnShowFinished").data("show-finished", true);
+            $("#btnShowFinished").text('Show open');
+            // load open notes
+            // ???
+        }
+    });
+
+    // finish note checkbox
+    $("body").on("change", ".checkboxFinishNote", function (event) {
+        event.preventDefault();
+        // mark note as finished
         // ???
-    } else {
-        $("#btnShowFinished").data("show-finished", true);
-        $("#btnShowFinished").text('Show open');
-        // load open notes
+    });
+
+    // select skin dropdown
+    $("body").on("change", "#btnSelectSkin", function (event) {
+        event.preventDefault();
+        // set body class
+        document.body.className = $(this).val();
+        // save selected skin
         // ???
-    }
-});
-
-// finish note checkbox
-$("body").on("change", ".checkboxFinishNote", function(event) {
-    event.preventDefault();
-    // mark note as finished
-    // ???
-});
-
-// select skin dropdown
-$("body").on("change", "#btnSelectSkin", function(event) {
-    event.preventDefault();
-    // set body class
-    document.body.className = $(this).val();
-    // save selected skin
-    // ???
-});
+    });
+})();
--- webapp/notes/public/javascripts/note.js
+++ webapp/notes/public/javascripts/note.js
@@ -1,76 +1,64 @@
-'use strict'
+;(function () {
+    'use strict'
 
-// update note
-$("body").on("click", "#btnSaveNote", function(event){
-    event.preventDefault();
-
-    var formData = {
-        title: $("#in-title").val(),
-        content: $("#in-description").val(),
-        priority: $("#in-priority").val(),
-        finishDate: $("#in-date").val()
-    };
-
-    // rest service
-    httpService.put($(this).data("urlpath"), formData, function (result) {
-        console.log(result);
-        window.location = "/";
-    }, function (result) {
-        console.log("Error: ", result);
-    });
-});
-
-// remove note
-$("body").on("click", "#btnRemoveNote", function(event) {
-    event.preventDefault();
-
-    // rest service
-    httpService.delete($(this).data("urlpath"), function (result) {
-        console.log(result);
-        window.location = "/";
-    }, function (result) {
-        console.log("Error: ", result);
-    });
-});
-
-// cancel note
-$("body").on("click", "#btnCancelNote", function(event) {
-    event.preventDefault();
-
-    // goto root
-    window.location = '/';
-});
-
-// set priority
-function iconPriorityHandler(event) {
-    event.preventDefault();
-    var svgElement = event.srcElement.tagName === "svg" ? event.srcElement : event.srcElement.parentElement;
-
-    if (svgElement.tagName === "svg") {
-        myTask.data.priority = svgElement.getAttribute("data-priority");
-        renderPriorityIconHTML(myTask.data.priority);
+    // render priority icons active state
+    function renderPriorityIcons() {
+        $(".icon-priority").each(function (index, value) {
+            if (index < $("#in-priority").val()) {
+                $(value).addClass("active");
+            } else {
+                $(value).removeClass("active");
+            }
+        });
     }
-};
 
-// render priority html
-function renderPriorityIconHTML(priority) {
-    var icons = document.querySelectorAll(".icon-priority");
+    renderPriorityIcons();
 
-    icons.forEach(function (icon, index) {
-        if (index < this.priority) {
-            icon.className.baseVal = "icon-priority active"
-        } else {
-            icon.className.baseVal = "icon-priority"
-        }
-    }, {priority: priority});
-}
-//renderPriorityIconHTML(myTask.data.priority);
+    // set priority (hidden input field)
+    $("body").on("click", ".icon-priority", function (event) {
+        event.preventDefault();
+        // add value to hidden field
+        $("#in-priority").val($(this).data("priority"));
+        // update priority icons active state
+        renderPriorityIcons();
+    });
 
-//select app skin
-function updateSkin() {
-    document.body.className = storageConfig.getSkin()
-};
+    // update note
+    $("body").on("click", "#btnSaveNote", function (event) {
+        event.preventDefault();
 
-// set priority
-var iconPriority = document.querySelector("#icon-priority-wrapper");
-iconPriority.addEventListener("click", iconPriorityHandler);
+        var formData = {
+            title: $("#in-title").val(),
+            content: $("#in-description").val(),
+            priority: $("#in-priority").val(),
+            finishDate: $("#in-date").val()
+        };
+
+        // rest service
+        httpService.put($(this).data("urlpath"), formData, function (result) {
+            console.log(result);
+            window.location = "/";
+        }, function (result) {
+            console.log("Error: ", result);
+        });
+    });
+
+    // remove note
+    $("body").on("click", "#btnRemoveNote", function (event) {
+        event.preventDefault();
+        // rest service
+        httpService.delete($(this).data("urlpath"), function (result) {
+            console.log(result);
+            window.location = "/";
+        }, function (result) {
+            console.log("Error: ", result);
+        });
+    });
+
+    // cancel note
+    $("body").on("click", "#btnCancelNote", function (event) {
+        event.preventDefault();
+        // goto root
+        window.location = '/';
+    });
+})();
