ffbcbdb4e599c6391883e571007bfe7b411bf185
Stefan Eugster <stefan.eugster@gmail.com>
2016-06-19 10:52:18 +0200

    extend handlebars formatDate helper with words feature

--- webapp/notes/public/javascripts/handlebarsHelper.js
+++ webapp/notes/public/javascripts/handlebarsHelper.js
@@ -6,7 +6,21 @@
      */
     Handlebars.registerHelper("formatDate", function (datetime, format) {
         var myDate = new Date(datetime);
-        return myDate.getDate() + '.' + (myDate.getMonth() + 1) + '.' + myDate.getFullYear();
+        var currentDate = new Date();
+        var timeDifference = myDate - currentDate;
+        var dayDifference = Math.ceil(timeDifference / (1000 * 3600 * 24));
+
+        if (format === "words") {
+            if (dayDifference === 0) {
+                return "Today";
+            } else if (dayDifference === 1) {
+                return "Tomorrow"
+            } else if (dayDifference) {
+                return "Somtime"
+            }
+        } else {
+            return myDate.getDate() + '.' + (myDate.getMonth() + 1) + '.' + myDate.getFullYear();
+        }
     });
 
     /**
